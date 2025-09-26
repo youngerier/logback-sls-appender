1dc94d7ea12d86dcead93e1b7609d3b91da98db3
wangping <wangping222@qbitnetwork.com>
2025-09-26 10:45:36 +0800

    feat: enhance SlsProtobufAppender with performance optimizations and error handling

---
+++ src/main/java/io/github/youngerier/logback/sls/appender/AsyncSlsProtobufAppender.java
@@ -0,0 +1,106 @@
+package io.github.youngerier.logback.sls.appender;
+
+import ch.qos.logback.classic.AsyncAppender;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.spi.AppenderAttachable;
+import ch.qos.logback.core.spi.AppenderAttachableImpl;
+
+/**
+ * 异步SLS Protobuf日志Appender
+ * 使用Logback的AsyncAppender机制包装SlsProtobufAppender，提供更好的性能
+ */
+public class AsyncSlsProtobufAppender extends AsyncAppender {
+    
+    private SlsProtobufAppender slsProtobufAppender;
+    
+    // SLS配置参数
+    private String endpoint;
+    private String accessKeyId;
+    private String accessKeySecret;
+    private String project;
+    private String logStore;
+    
+    // 批处理配置参数
+    private int batchSize;
+    private int queueSize;
+    private int flushIntervalMs;
+    private boolean useBase64Encoding;
+    
+    @Override
+    public void start() {
+        // 创建并配置SlsProtobufAppender
+        slsProtobufAppender = new SlsProtobufAppender();
+        slsProtobufAppender.setEndpoint(endpoint);
+        slsProtobufAppender.setAccessKeyId(accessKeyId);
+        slsProtobufAppender.setAccessKeySecret(accessKeySecret);
+        slsProtobufAppender.setProject(project);
+        slsProtobufAppender.setLogStore(logStore);
+        
+        // 设置批处理参数（如果已配置）
+        if (batchSize > 0) {
+            slsProtobufAppender.setBatchSize(batchSize);
+        }
+        if (queueSize > 0) {
+            slsProtobufAppender.setQueueSize(queueSize);
+        }
+        if (flushIntervalMs > 0) {
+            slsProtobufAppender.setFlushIntervalMs(flushIntervalMs);
+        }
+        slsProtobufAppender.setUseBase64Encoding(useBase64Encoding);
+        
+        // 设置上下文并启动SlsProtobufAppender
+        slsProtobufAppender.setContext(getContext());
+        slsProtobufAppender.start();
+        
+        // 将SlsProtobufAppender添加到AsyncAppender
+        addAppender(slsProtobufAppender);
+        
+        // 启动AsyncAppender
+        super.start();
+    }
+    
+    @Override
+    public void stop() {
+        super.stop();
+        slsProtobufAppender.stop();
+    }
+    
+    // SLS配置参数的getter/setter
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+    
+    public void setAccessKeyId(String accessKeyId) {
+        this.accessKeyId = accessKeyId;
+    }
+    
+    public void setAccessKeySecret(String accessKeySecret) {
+        this.accessKeySecret = accessKeySecret;
+    }
+    
+    public void setProject(String project) {
+        this.project = project;
+    }
+    
+    public void setLogStore(String logStore) {
+        this.logStore = logStore;
+    }
+    
+    // 批处理配置参数的getter/setter
+    public void setBatchSize(int batchSize) {
+        this.batchSize = batchSize;
+    }
+
+    @Override
+    public void setQueueSize(int queueSize) {
+        this.queueSize = queueSize;
+    }
+    
+    public void setFlushIntervalMs(int flushIntervalMs) {
+        this.flushIntervalMs = flushIntervalMs;
+    }
+    
+    public void setUseBase64Encoding(boolean useBase64Encoding) {
+        this.useBase64Encoding = useBase64Encoding;
+    }
+}
