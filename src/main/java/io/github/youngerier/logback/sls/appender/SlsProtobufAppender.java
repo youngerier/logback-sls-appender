33a2187a2c35769ae3102a3e0107e479430f11de
wangping <wangping222@qbitnetwork.com>
2025-09-26 10:13:04 +0800

    feat: 初始化项目基础结构和SLS日志appender实现
    添加项目基础文件包括.gitignore、.gitattributes、pom.xml等
    实现SLS Protobuf日志appender核心功能
    配置Maven wrapper和项目依赖

---
+++ src/main/java/io/github/youngerier/logback/sls/appender/SlsProtobufAppender.java
@@ -0,0 +1,61 @@
+package io.github.youngerier.logback.sls.appender;
+
+import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import logging.LogMessage;
+// 生成的protobuf类
+
+import com.aliyun.openservices.log.Client;
+import com.aliyun.openservices.log.common.LogItem;
+
+import java.util.Collections;
+
+public class SlsProtobufAppender extends AppenderBase<ILoggingEvent> {
+
+    private String endpoint;
+    private String accessKeyId;
+    private String accessKeySecret;
+    private String project;
+    private String logStore;
+
+    private transient Client client;
+
+    @Override
+    public void start() {
+        super.start();
+        this.client = new Client(endpoint, accessKeyId, accessKeySecret);
+    }
+
+    @Override
+    protected void append(ILoggingEvent event) {
+        try {
+            // 构建protobuf对象
+            LogMessage logMessage = LogMessage.newBuilder()
+                    .setLevel(event.getLevel().toString())
+                    .setThread(event.getThreadName())
+                    .setLogger(event.getLoggerName())
+                    .setMessage(event.getFormattedMessage())
+                    .setTimestamp(event.getTimeStamp())
+                    .build();
+
+            byte[] data = logMessage.toByteArray();
+
+            // 封装为 SLS LogItem
+            LogItem item = new LogItem();
+            item.PushBack("protobuf", new String(data));
+            // ⚠️ SLS 日志存储 key/value，需要考虑是否 base64 或 hex 编码保存
+
+            client.PutLogs(project, logStore, "", Collections.singletonList(item));
+
+        } catch (Exception e) {
+            addError("Failed to send log to SLS", e);
+        }
+    }
+
+    // getter/setter for logback.xml 配置
+    public void setEndpoint(String endpoint) { this.endpoint = endpoint; }
+    public void setAccessKeyId(String accessKeyId) { this.accessKeyId = accessKeyId; }
+    public void setAccessKeySecret(String accessKeySecret) { this.accessKeySecret = accessKeySecret; }
+    public void setProject(String project) { this.project = project; }
+    public void setLogStore(String logStore) { this.logStore = logStore; }
+}
